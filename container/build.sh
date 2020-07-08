68ae0b8e4e5a41c5df70ffed68887870dfb1a807
C. Guychard <christophe@certificare.fr>
2020-07-08 15:47:18 +0200

    [FIX] missing dependencies

--- container/build.sh
+++ container/build.sh
@@ -6,6 +6,10 @@ export LANG=en_US.utf8
 groupadd vault
 adduser --no-create-home --disabled-password --shell /usr/sbin/nologin --gecos "" vault vault
 
+# install gnupg to enable adding of hashicorp keys
+apt-get update
+apt-get install -qy --no-install-recommends gnupg gnupg-agent
+
 # Install Vault
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
