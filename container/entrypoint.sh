99d0dec7db4a1198cbbdff73e8ad1a585cbf9404
C. Guychard <christophe@certificare.fr>
2020-08-18 20:34:18 +0200

    [ADD] add a way to provide multiple alternative names for certificate generation

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -5,10 +5,15 @@ PATH=/sbin:/usr/sbin:/bin:/usr/bin
 #set -x
 
 start() {
-    if [ ! -f "/container/config/vault/certs/vault.crt" ]; then
+    if [[ ! -f "/container/config/vault/certs/vault.crt" && -n "${VAULT_X509_SUBJECT}" ]]; then
         # Build self-signed Certificate
         cd /container/config/vault/certs
-        chpst -u vault openssl req -newkey rsa:4096 -days 1001 -nodes -x509 -subj ${VAULT_X509_SUBJECT} -keyout "vault.key" -out "vault.crt"
+        if [ -n "${VAULT_X509_ALTNAMES}"]; then
+            chpst -u vault openssl req -newkey rsa:4096 -days 1001 -nodes -x509 -subj ${VAULT_X509_SUBJECT} -addext "subjecAltName = ${VAULT_X509_ALTNAMES}" -keyout "vault.key" -out "vault.crt"
+        else
+            chpst -u vault openssl req -newkey rsa:4096 -days 1001 -nodes -x509 -subj ${VAULT_X509_SUBJECT} -keyout "vault.key" -out "vault.crt"
+        fi
+
     fi
 
     exec runsvdir -P /container/services
